13263303c68e9e519328b9a0e71ff8d6dce31dee
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2021-05-17 03:17:01 +0200

    Create TripRepository

--- src/main/java/service/model/User.java
+++ src/main/java/service/model/User.java
@@ -1,4 +1,13 @@
 package service.model;
 
 public class User {
+    private int id;
+    private String name;
+    private String image;
+
+    public User(int id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
 }
